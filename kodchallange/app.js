d8ae883131d05a7e1a4edb67a8df41222c4d6e8f
naimetysz <nnaimetuysuz@gmail.com>
2022-08-31 18:31:18 +0300

    kod cozumleri eklendi

--- kodchallange/app.js
+++ kodchallange/app.js
@@ -1,85 +1,73 @@
-1. //Bölüm Loops
-
-//! Q-1
+1. //! Q-1
 //? Ask the user to input a number. Check the input of the user, if it is not a valid number
 //? then keep getting input from the user until provides a valid number.
-const valinN =()=>{
-    let num = prompt("enter a valid number")
-    if(Number(num)){
-        return "it is a valid number"
-        else {
-            return validN()
 
-        }
-    }        //recursion
+// const validNumber = () => {
+//     let num = prompt("Enter a valid number")
 
-}
-console.log(validN());
+//     if (Number(num)){
+//         console.log("It is a valid number") 
+//     } else {
+//         return validNumber()              // recursion
+//     }
+// }
 
+// console.log(validNumber());
 
 
 //! Q-2
 //? Write a JavaScript program to compute the greatest common divisor (GCD) (OBEB) of two positive integers.
-const findGcd = (a,b) => {
-    let gcd;
-    if(a==0|| b==0){
-        return 'please enter a number'
 
-    }
-    for (let i =1; i<8 i++){
-        
-    }
+// const findGcd = (num1, num2) => {
+//     let gcd;
 
-}
-console.log(findGcd(3,6))
+//     if (num1 === 0 || num2 === 0){
+//         return 'Please enter a valid number'
+//     }
+
+//     for (let i = 1; i <= num1 && i <= num2; i++){
+//         if (num1 % i === 0 && num2 % i === 0){
+//             gcd = i;
+//         }
+//     }
+
+//     return gcd
+// }
+
+// console.log(findGcd(3,6));
 
 //! Q-3
 //? Write a code that displays the factors (tam bolen) of a number entered by the user.
 
+// const factor = (num) => {
+//     let arr = []
+
+//     for (let i = 1; i <= num; i++){
+//         if (num % i === 0){
+//             arr.push(i, -i)
+//         }
+//     }
+
+//     return arr;
+// }
+
+// console.log(factor(12));
+// console.log(factor(13));
+
 //! Q-4
 //? Write a code to calculate the factorial of a number entered by the user.
 
+// const factorial = (num) => {
+//     if (num < 0) return -1;
+
+//     if (num === 0){
+//         return 1
+//     } else {
+//         return num * factorial(num - 1)
+//     }
+// }
+
+// console.log(factorial(3));
+
 //! Q-5
 //? Write a code to find numbers that are divisible by 3 and 5, between 100 and 1000.
-
-
-//! Q-6
-//? Write a code that prints a multiplication table of numbers 1 to 10.
-
-
-//! Q-7
-//? Write a code to draw Whiteball and Powerball numbers for the Powerball lottery game. 
-//? 5 Whiteball numbers are chosen from 01 to 69 and 1 Powerball number is chosen from 1 to 26.
-
-
-
-2. //Bölüm Functions
-
-//! Q-1
-//? Write a Function that takes 2 parameters (amount and rate) to convert currency.
-
-//! Q-2
-//? Write a JavaScript function that accepts an argument and returns the type.
-
-//! Q-3
-//? You are given a string and character. Write a function to check the string and find how many
-//? times this character occurs in this string.
-//? Example:
-//? str = "Clarusway"         char = "a"       ===> result = 2 as "a" occurs 2 times in Clarusway
-
-//! Q-4
-//? Write a JS code to display Perfect Numbers from 1 to 1000.
-//? A perfect number is a positive integer that is equal to the sum of its positive divisors, excluding the number itself.
-//? For instance, 6 has divisors 1, 2 and 3 (excluding itself), and 1 + 2 + 3 = 6, so 6 is a perfect number.
-
-
-//! Q-5
-//? Develop a guessing game. Randomly generate a number between 0-100. Ask the user to guess that number.
-//? The user should have 5 chance to guess the number.
-//? If the number entered by the user:
-//? is greater than the random number then warn user to decrease,
-//? is less than the random number then warn user to increase,
-//? is equal to the random number then congratulate the user and tell in how many guesses did the user find the number
-//? If user can't find the number in 5 guess then game is lost and display the random number.
-//? Hint: you can use this code to generate random number 0-100
-//? const winingNumber = Math.round(Math.random() * 100);
