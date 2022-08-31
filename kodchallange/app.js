1db3b46cd94f3124cd58f41427d5627849d8caef
naimetysz <nnaimetuysuz@gmail.com>
2022-08-31 18:53:35 +0300

    loop cozumu eklendi

--- kodchallange/app.js
+++ kodchallange/app.js
@@ -1,4 +1,4 @@
-1. //! Q-1
+//! Q-1
 //? Ask the user to input a number. Check the input of the user, if it is not a valid number
 //? then keep getting input from the user until provides a valid number.
 
@@ -71,3 +71,53 @@
 
 //! Q-5
 //? Write a code to find numbers that are divisible by 3 and 5, between 100 and 1000.
+
+// const divisible = () => {
+//     let arr = []
+
+//     for (let i =100; i <= 1000; i++){
+//         if (!(i % 3) && !(i % 5)){
+//             arr.push(i)
+//         }
+//     }
+
+//     return arr
+// }
+
+// console.log(divisible())
+
+//! Q-6
+//? Write a code that prints a multiplication table of numbers 1 to 10.
+
+// const multiplication = () => {
+//     let arr = []
+    
+//     for (let i = 1; i <= 10; i++){
+//         arr.push(`multiplication table of ${i}`)
+//         for (let k = 0; k <= 10; k++){
+//             arr.push(`${i} * ${k} = ${i*k}`)
+//         }
+//     }
+
+//     return arr
+// }
+
+// console.log(multiplication())
+
+//! Q-7
+//? Write a code to draw Whiteball and Powerball numbers for the Powerball lottery game. 
+//? 5 Whiteball numbers are chosen from 01 to 69 and 1 Powerball number is chosen from 1 to 26.
+
+// const powerball = () => {
+//     let powerball = Math.floor(Math.random() * 26) + 1
+//     let whiteball = []
+
+//     for (let i = 1; i <= 5; i++){
+//         let random = Math.floor(Math.random() * 69) + 1
+//         whiteball.push(random)
+//     }
+
+//     return `${whiteball.join(', ')}, ${powerball}`
+// }
+
+// console.log(powerball())
